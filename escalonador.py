b6597219699eb6a0eaf112eabe93e4a787d6f497
99heitor <99heitor@gmail.com>
2015-12-04 19:09:07 -0300

    Transformando o Escalonador em uma classe.

--- escalonador.py
+++ escalonador.py
@@ -12,54 +12,61 @@ from priority import Priority
 from fcfs import FCFS
 from sjfp import SJFP
 
+class Escalonador():
 
-def selecionarAlgoritmo(args,processos):
-	try:
-		nomeAlgoritmo = args[2].lower()
-	except IndexError:
-		print "Algoritmo não especificado\nFinalizando..."
-		sys.exit()
-	if nomeAlgoritmo == "priority":
-		return Priority(processos)
-	elif nomeAlgoritmo == "priorityp":
-		return PriorityP(processos)
-	elif nomeAlgoritmo == "fcfs":
-		return FCFS(processos)
-	elif nomeAlgoritmo == "sjf":
-		return SJF(processos)
-	elif nomeAlgoritmo == "sjfp":
-		return SJFP(processos)
-	elif nomeAlgoritmo == "rr":
+	def __init__(self, args):
+		self.args = args
+		self.processos = []
+		self.algoritmo = self.selecionarAlgoritmo()
+
+	def executar(self):
 		try:
-			return RoundRobin(processos,int(args[3]))
+			f = open(self.args[1], 'rt')
+		except IOError:
+			print "O arquivo de entrada não existe.\nFinalizando..."
+			sys.exit()
+		reader = inputReader(f)
+		a = reader.executar()
+
+		for row in a:
+			self.processos.append(Processo(row))
+
+		algoritmo = selecionarAlgoritmo()
+		
+		algoritmo.executar()
+
+		reporter = outputWriter(self.args,self.processos)
+		reporter.executar()
+
+	def selecionarAlgoritmo(self):
+		try:
+			nomeAlgoritmo = self.args[2].lower()
 		except IndexError:
-			print "Defina o quantum (terceiro argumento) para o algoritmo RoundRobin\nFinalizando..."
+			print "Algoritmo não especificado\nFinalizando..."
 			sys.exit()
-		except ValueError:
-   			print "Valor do quantum não é um inteiro\nFinalizando"
-			sys.exit()
-	 
-	print "Algoritmo não existe\nFinalizando..."
-	sys.exit()
-
-def main():
-	try:
-		f = open(sys.argv[1], 'rt')
-	except IOError:
-		print "O arquivo de entrada não existe.\nFinalizando..."
+		if nomeAlgoritmo == "priority":
+			return Priority(self.processos)
+		elif nomeAlgoritmo == "priorityp":
+			return PriorityP(self.processos)
+		elif nomeAlgoritmo == "fcfs":
+			return FCFS(self.processos)
+		elif nomeAlgoritmo == "sjf":
+			return SJF(self.processos)
+		elif nomeAlgoritmo == "sjfp":
+			return SJFP(self.processos)
+		elif nomeAlgoritmo == "rr":
+			try:
+				return RoundRobin(self.processos,int(self.args[3]))
+			except IndexError:
+				print "Defina o quantum (terceiro argumento) para o algoritmo RoundRobin\nFinalizando..."
+				sys.exit()
+			except ValueError:
+	   			print "Valor do quantum não é um inteiro\nFinalizando"
+				sys.exit()
+		 
+		print "Algoritmo não existe\nFinalizando..."
 		sys.exit()
-	reader = inputReader(f)
-	a = reader.executar()
-	processos = []
-	for row in a:
-		processos.append(Processo(row))
-
-	algoritmo = selecionarAlgoritmo(sys.argv,processos)
-	
-	algoritmo.executar()
-
-	reporter = outputWriter(sys.argv,processos)
-	reporter.executar()
 
 if __name__ == "__main__":
-     main()	
+     e = Escalonador(sys.argv)
+     e.executar
